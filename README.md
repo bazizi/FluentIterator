96e791c1d9f481b9af77659f0c00ef3d193c4117
bazizi <bazizi@github.com>
2024-07-07 16:42:25 -0700

    improve code example

--- README.md
+++ README.md
@@ -5,17 +5,14 @@ An experimental project which attempts to implement Rust-like fluent iterator ad
 
 ```cpp
 FluentIterator<std::vector<int>> v = {1, 2, 3, 4, 5, 6};
-auto result = v.map([](auto const &item) { return item * 20; })
-                        .print() // 20, 40, 60, 80, 100, 120
-                        .filter([](auto const &item)
-                                { return item % 3 != 0; })
-                        .print() // 20, 40, 80, 100
-                        .map([](auto const &item)
-                        { return item / 4; })
-                        .print() // 5, 10, 20, 25
-                        .filter([](auto const &item)
-                                { return item % 4 == 0; })
-                        .print() // 20
+v = v.map([](auto const &item) { return item * 20; })
+        .print() // 20, 40, 60, 80, 100, 120
+        .filter([](auto const &item) { return item % 3 != 0; })
+        .print() // 20, 40, 80, 100
+        .map([](auto const &item) { return item / 4; })
+        .print() // 5, 10, 20, 25
+        .filter([](auto const &item) { return item % 4 == 0; })
+        .print() // 20
         ;
 ```
 
--- main.cpp
+++ main.cpp
@@ -5,41 +5,40 @@
 
 int main()
 {
-    {
-        FluentIterator<std::vector<int>> v = {1, 2, 3, 4, 5, 6};
+        {
+                FluentIterator<std::vector<int>> v = {1, 2, 3, 4, 5, 6};
+                v = v.map([](auto const &item)
+                          { return item * 20; })
+                        .print() // 20, 40, 60, 80, 100, 120
+                        .filter([](auto const &item)
+                                { return item % 3 != 0; })
+                        .print() // 20, 40, 80, 100
+                        .map([](auto const &item)
+                             { return item / 4; })
+                        .print() // 5, 10, 20, 25
+                        .filter([](auto const &item)
+                                { return item % 4 == 0; })
+                        .print() // 20
+                        .collect();
 
-        auto result = v.map([](auto const &item)
-                            { return item * 20; })
-                          .print() // 20, 40, 60, 80, 100, 120
-                          .filter([](auto const &item)
-                                  { return item % 3 != 0; })
-                          .print() // 20, 40, 80, 100
-                          .map([](auto const &item)
-                               { return item / 4; })
-                          .print() // 5, 10, 20, 25
-                          .filter([](auto const &item)
-                                  { return item % 4 == 0; })
-                          .print() // 20
-            ;
+                std::cout << std::endl;
+        }
 
-        std::cout << std::endl;
-    }
-
-    {
-        FluentIterator<std::list<int>> v = {1, 2, 3, 4, 5, 6};
-
-        auto result = v.map([](auto const &item)
-                            { return item * 20; })
-                          .print() // 20, 40, 60, 80, 100, 120
-                          .filter([](auto const &item)
-                                  { return item % 3 != 0; })
-                          .print() // 20, 40, 80, 100
-                          .map([](auto const &item)
-                               { return item / 4; })
-                          .print() // 5, 10, 20, 25
-                          .filter([](auto const &item)
-                                  { return item % 4 == 0; })
-                          .print() // 20
-            ;
-    }
+        {
+                FluentIterator<std::list<int>> v = {1, 2, 3, 4, 5, 6};
+                v = v.map([](auto const &item)
+                          { return item * 20; })
+                        .print() // 20, 40, 60, 80, 100, 120
+                        .filter([](auto const &item)
+                                { return item % 3 != 0; })
+                        .print() // 20, 40, 80, 100
+                        .map([](auto const &item)
+                             { return item / 4; })
+                        .print() // 5, 10, 20, 25
+                        .filter([](auto const &item)
+                                { return item % 4 == 0; })
+                        .print()
+                        .collect() // 20
+                    ;
+        }
 }
